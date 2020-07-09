dfdf5297ac0e54bfb56b74dd6cb5a53f2a78dd01
PaulKorotenko <dev.paul@mail.ru>
2020-07-09 18:48:25 +0300

    Task 3 done

---
+++ task_3.py
@@ -0,0 +1,15 @@
+# Запрашиваем целое число у пользователя
+usr_num = input('Введите целое число: ')
+# Одиночное число
+single_num = int(usr_num)
+# Двойное число
+double_num = int(usr_num * 2)
+# Тройное число
+triple_num = int(usr_num * 3)
+# Запрашиваемая сумма
+total = single_num + double_num + triple_num
+# Выводим полученную сумму в stdout
+print(f'Сумма: {total}')
+
+
+# Written by PaulKorotenko
