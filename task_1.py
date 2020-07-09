87bcae7054bccb46f050b6cae997f2dd66ec0036
PaulKorotenko <dev.paul@mail.ru>
2020-07-09 18:47:59 +0300

    Task 1 done

---
+++ task_1.py
@@ -0,0 +1,15 @@
+# Создаёи интовую переменную number и выводим её в stdout
+number = 100
+print('number:', number)
+# Создаёи стринговую переменную string и выводим её в stdout
+string = 'Hello'
+print('string:', string)
+# Запрашиваем ввод у пользователя, сохраняем его в переменную usr_str и выводим
+usr_str = input('Введите строку: ')
+print('usr_str:', usr_str)
+# Запрашиваем ввод у пользователя, конвертируем его из строки в число, сохраняем в переменную usr_num и выводим
+usr_num = int(input('Введите целое число: '))
+print('usr_num:', usr_num)
+
+
+# Written by PaulKorotenko
