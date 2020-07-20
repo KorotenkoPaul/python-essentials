fe995e793c050be021df0465c78730399e4074fa
PaulKorotenko <dev.paul@mail.ru>
2020-07-20 13:46:46 +0300

    Task 1 done

---
+++ task_1.py
@@ -0,0 +1,9 @@
+# Создаём список с помощью литерала списков
+my_list = [3.14, 'string', {1: 'one', 2: 'two'}]
+# Обходим каждый элемент списка
+for item in my_list:
+    # Вывод в stdout
+    print(f'{item} => {type(item)}')
+
+
+# Written by PaulKorotenko
